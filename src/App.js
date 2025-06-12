2e4b1afe73db1b53f9f440362ef572c13635cb96
YasminPadilha <mindopadilha@hotmail.com>
2025-06-12 11:43:12 +1000

    add router & watchlist component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import logo from "./logo.svg";
 import "./styles.css";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <MoviesGrid />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
